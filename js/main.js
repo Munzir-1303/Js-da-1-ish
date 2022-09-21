abfa27720941def31d64e085e43403bd12450673
Munzir-1303 <usanpubgbot@gmail.com>
2022-09-21 14:05:16 +0300

    Js da 1-ish

---
+++ js/main.js
@@ -0,0 +1,41 @@
+var Jins = prompt ("Jinsingiz👫");
+console.log(Jins);
+
+var Name = prompt ("Ismingiz👍");
+console.log(Name);
+
+var Age = prompt ("Yoshingiz🤷‍♀️");
+console.log(Age);
+
+var From = prompt ("Qayerdansan😊");
+console.log(From);
+
+var Read = prompt ("Qayerda o'qiysan📖");
+console.log(Read);
+
+var Hobby = prompt ("Qiziqishing🎮");
+console.log(Hobby);
+
+var Love = prompt ("Sevgiling bormi😍");
+console.log(Love);
+
+var Numbers = prompt ("Nomering🤙🏻");
+console.log(Numbers);
+
+var Sleep = prompt ("Uxlaysanmi O'zi😫");
+console.log(Sleep);
+
+
+var javob = [
+    "Jinsingiz👫:" + " " +  Jins + "\n" +
+    "Ismingiz👍:" + " " +  Name + "\n" +
+    "Yoshingiz🤷‍♀️:" + " " +  Age + "\n" +
+    "Qayerdansan😊:" + " " +  From + "\n" +
+    "Qayerda o'qiysan📖:" + " " +  Read + "\n" +
+    "Qiziqishing🎮:" + " " +  Hobby + "\n" +
+    "Sevgiling bormi😍:" + " " +  Love + "\n" +
+    "Nomering🤙🏻:" + " " +  Numbers + "\n" +
+    "Uxlaysanmi😫:" + " " +  Sleep + "\n"
+]
+
+alert(javob)
